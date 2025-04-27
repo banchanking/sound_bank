c8c0c93b542d4cb7a18fe4704ac68c407e200790
hikim123 <jinjiang1128@naver.com>
2025-04-27 18:48:36 +0900

    로그인 알러창수정

--- front-react/src/inquire/InquireTransfer.js
+++ front-react/src/inquire/InquireTransfer.js
@@ -27,10 +27,18 @@ function CheckTx() {
   useEffect(() => {
     const id = getCustomerID();
     if (!id) {
-      alert('로그인이 필요합니다.');
-      navigate('/login');
-      return;
+      if (!id) {
+        const goLogin = window.confirm(
+          "로그인이 필요합니다. 로그인 페이지로 이동하시겠습니까?"
+        );
+        if (goLogin) {
+          navigate("/login");
+        } else {
+          navigate("/");
+        }
+        return;      
     }
+  }
     setCustomerId(id);
 
     RefreshToken.get(`/accounts/allAccount/${id}`)
